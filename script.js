ac96b547fbf276fdf5c75b416372ec2bbb315126
Subhash Kumawat <subhashkumawat569@gmail.com>
2026-06-30 11:03:23 +0530

    Day 6: Added Dark/Light Mode

--- script.js
+++ script.js
@@ -468,3 +468,43 @@ window.addEventListener("load",()=>{
 setTimeout(hackerBoot,500);
 
 });
+
+/* ===========================
+   Theme Toggle
+=========================== */
+
+const themeBtn = document.getElementById("theme-toggle");
+
+// Saved theme load karo
+if(localStorage.getItem("theme") === "light"){
+
+    document.body.classList.add("light-mode");
+
+    themeBtn.textContent = "☀️";
+
+}else{
+
+    themeBtn.textContent = "🌙";
+
+}
+
+// Button click
+themeBtn.addEventListener("click",()=>{
+
+    document.body.classList.toggle("light-mode");
+
+    if(document.body.classList.contains("light-mode")){
+
+        localStorage.setItem("theme","light");
+
+        themeBtn.textContent="☀️";
+
+    }else{
+
+        localStorage.setItem("theme","dark");
+
+        themeBtn.textContent="🌙";
+
+    }
+
+});
